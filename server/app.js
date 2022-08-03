9b5e4ed6d7a172cc1478ca56d0a7082387600531
agiskorska <skorskaga@gmail.com>
2022-08-03 11:28:40 +0100

    Fix code - start server

--- server/app.js
+++ server/app.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 
 
 
 app.get('/owner/', (req, res) => {
-    res.status(200).send({message: 'fetched data of all owners'})
+    res.status(200).send({message: 'fetched data of all owners'});
 
 });
 
 app.get('/owner/:id', (req, res) => {
+    res.status(200).send({message: 'fetched data of owner ' + req.params.id});
 
-    res.status(200).send({message: 'fetched data of owner ' + req.params.id})
 });
 
 app.post('/owner/', (req, res) => {
-    res.status(201).send({message: 'created a new owner'})
+    res.status(201).send({message: 'created a new owner'});
 });
 
 app.put('/owner/:id', (req, res) => {
-    res.status(201).send({message: 'changed owner ' + req.params.id})
+    res.status(201).send({message: 'changed owner ' + req.params.id});
 
 });
 
 app.get('/house/:id', (req, res) => {
-    res.status(200).send({message: "fetched the house" + req.params.id})
+    res.status(200).send({message: "fetched the house" + req.params.id});
 });
 
 app.get('/postcode/:id/', (req, res) => {
@@ -32,10 +32,12 @@ app.get('/postcode/:id/', (req, res) => {
     const maxAge = req.query.maxAge
     const size = req.query.size
     const message = 'fetched all houses within ' + req.params.id + ' area, with people between ' + minAge + ' and ' + maxAge + ' and households of ' + size
-    res.status(200).send({message})    
+    res.status(200).send({message: message});
 });
 
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
+
+module.exports = app;
